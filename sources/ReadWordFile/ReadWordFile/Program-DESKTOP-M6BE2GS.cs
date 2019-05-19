59c1d26e278572b9bd47e966c7a59c72d4e24969
huydinh3010 <huydinh3010@gmail.com>
2019-05-19 17:49:21 +0700

    modify word file

---
+++ sources/ReadWordFile/ReadWordFile/Program-DESKTOP-M6BE2GS.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
+
+namespace ReadWordFile
+{
+    interface Animal
+    {
+        void showInfo(); // phương thức thuần ảo, chỉ khai báo mà ko triển khai, phải để trong class abstract
+        void keu();
+        int abc
+        {
+            get;
+            set;
+        }
+
+    }
+
+    class Dog : Animal
+    {
+
+        public void keu()
+        {
+
+        }
+
+        public void showInfo()
+        {
+            Console.WriteLine("hahasdkhfjkashfasf");
+        }
+
+        public Dog()
+        {
+
+        }
+        public int abc
+        {
+            get { return 0; }
+            set { }
+        }
+
+        public Dog(int height, int weight, int legs)
+        {
+
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Application word = new Application();
+            //Document doc = word.Documents.Open("C:\\Users\\Nguyen Huy Dinh\\Desktop\\word_file.docx");
+            //String read = string.Empty;
+            //List<string> data = new List<string>();
+            //for (int i = 1; i <= doc.Paragraphs.Count; i++)
+            //{
+            //    string temp = doc.Paragraphs[i].Range.Text;
+            //    if (temp != string.Empty)
+            //        data.Add(temp);
+            //    Console.WriteLine(temp);
+            //}
+            //doc.Paragraphs[0].Range.Text = "a";
+
+            //doc.Paragraphs[0].Format.SpaceAfter = 24;
+            //doc.Content.Text = "Heading 1";
+            //doc.Content.Font.Bold = 1;
+            //doc.Content.Font.Size = 24;
+            //((_Document)doc).Close();
+            //((_Application)word).Quit();
+
+            // truyeenf tham so boi gia tri: truyen nhu binh thuong
+            // truyen tham so boi tham chieu: them ref truoc tham so vd swap(ref a, ref b)
+            // truyen boi output: them out vd swap(out a)
+            // nullable: kieu du lieu co ca gia tri null, int?
+            // toan tu ??: vd: a??b ,neu a == null thi tra ve b, ko thi tra ve a
+
+            // params, truyen nhieu tham so
+            // int[] arr = new int[] { 0, 1, 1 };
+            // string str = new string("abc");
+            // str = "hello";
+            // static class, chỉ chứa các biến tĩnh và phương thức tĩnh
+            // static function: chỉ có thể truy cập vào các thành viên tĩnh 
+            // constructor tĩnh: gọi 1 lần duy nhất khởi tạo cho toàn bộ class, ko có access specifier
+
+            Animal a = new Dog();
+            a.showInfo();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ sources/ReadWordFile/ReadWordFile/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+using System.Reflection;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace ReadWordFile
+{
+    class DBSQLServerUtils
+    {
+        public static SqlConnection GetDBConnection(string datasource, string database, string username, string password) 
+        {
+            // connection string
+            string connString = @"Data Source=" + datasource + ";Initial Catalog=" + database + ";Persist Security Info=True;User ID=" + username + ";Password=" + password;
+            return new SqlConnection(connString);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = DBSQLServerUtils.GetDBConnection("DESKTOP-BR7TD27\\SQLEXPRESS", "Phong_xet_nghiem", "sa", "30101998");
+
+            try
+            {
+                Console.Write("ID = ");
+                int id = Convert.ToInt32(Console.ReadLine());
+                connection.Open(); // khởi tạo kết nối đến SQL Server
+                SqlCommand cmd = connection.CreateCommand();
+                cmd.CommandText = "Select * from XetNghiem where ID = " + id;
+                DbDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    object oMissing = System.Reflection.Missing.Value;
+                    object oEndOfDoc = "\\endofdoc"; 
+                    Word._Application oWord;
+                    Word._Document oDoc;
+                    oWord = new Word.Application();
+                    oDoc = oWord.Documents.Add(ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+                    DateTime ngay_co_kq = new DateTime();
+                    while (reader.Read())
+                    {
+                        // KẾT QUẢ XÉT NGHIỆM
+                        oWord.Selection.Font.Bold = 1; // chữ đậm
+                        oWord.Selection.Font.Size = 24; // cỡ 24
+                        oWord.Selection.Font.Name = "Times New Roman"; // kiểu font chữ
+                        oWord.Selection.TypeText("KẾT QUẢ XÉT NGHIỆM"); // text
+                        oWord.Selection.Paragraphs.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter; // căn lề
+                        oWord.Selection.ParagraphFormat.SpaceAfter = 24; // khoảng cách đến dòng kế tiếp
+                        oWord.Selection.TypeText(Environment.NewLine); // dòng mới
+
+                        // Tên bệnh nhân: Nguyễn Văn A
+                        oWord.Selection.Font.Bold = 0; // bỏ chữ đậm
+                        oWord.Selection.Font.Size = 14; // cỡ 14
+                        oWord.Selection.Font.Underline = Word.WdUnderline.wdUnderlineSingle; // gạch dưới
+                        oWord.Selection.Font.Italic = 1; // in nghiêng
+                        oWord.Selection.Paragraphs.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft; // căn lề trái
+                        oWord.Selection.TypeText("Tên bệnh nhân:"); // tên bệnh nhân
+                        oWord.Selection.Font.Underline = Word.WdUnderline.wdUnderlineNone; // bỏ gạch dưới
+                        oWord.Selection.Font.Italic = 0; // bỏ in nghiêng
+                        oWord.Selection.TypeText(" " + reader.GetString(reader.GetOrdinal("TenBenhNhan"))); // tên lấy từ csdl
+                        oWord.Selection.ParagraphFormat.SpaceAfter = 8;
+                        oWord.Selection.TypeText(Environment.NewLine); 
+
+                        // Giới tính: Nam
+                        oWord.Selection.Font.Underline = Word.WdUnderline.wdUnderlineSingle;
+                        oWord.Selection.Font.Italic = 1;
+                        oWord.Selection.TypeText("Giới tính:");
+                        oWord.Selection.Font.Underline = Word.WdUnderline.wdUnderlineNone;
+                        oWord.Selection.Font.Italic = 0;
+                        oWord.Selection.TypeText(" " + reader.GetString(reader.GetOrdinal("GioiTinh")));
+                        oWord.Selection.TypeText(Environment.NewLine);
+
+                        // Năm sinh: 1998
+                        oWord.Selection.Font.Underline = Word.WdUnderline.wdUnderlineSingle;
+                        oWord.Selection.Font.Italic = 1;
+                        oWord.Selection.TypeText("Năm sinh:");
+                        oWord.Selection.Font.Underline = Word.WdUnderline.wdUnderlineNone;
+                        oWord.Selection.Font.Italic = 0;
+                        oWord.Selection.TypeText(" " + reader.GetInt32(reader.GetOrdinal("NamSinh")));
+                        oWord.Selection.ParagraphFormat.SpaceAfter = 24;
+                        oWord.Selection.TypeText(Environment.NewLine);
+
+                        ngay_co_kq = reader.GetDateTime(reader.GetOrdinal("NgayCoKetQua"));
+
+                    }
+                    reader.Close();
+                    // Bảng kết quả
+                    Word.Table oTable;
+                    Word.Range wrdRng = oDoc.Bookmarks.get_Item(ref oEndOfDoc).Range; // đặt vị trí tạo bảng
+                    oTable = oDoc.Tables.Add(wrdRng, 13, 2, ref oMissing, ref oMissing); // tạo bảng
+                    oTable.Borders.InsideLineStyle = Word.WdLineStyle.wdLineStyleSingle; // kiểu đường viền bên trong bảng
+                    oTable.Borders.OutsideLineStyle = Word.WdLineStyle.wdLineStyleSingle; // kiểu đường viền bên ngoài
+                    oTable.Range.ParagraphFormat.SpaceAfter = 6;
+                    oTable.Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                    oTable.Cell(1, 1).Range.Text = "Loại mẫu";
+                    oTable.Cell(1, 2).Range.Text = "Kết quả";
+                    cmd.CommandText = "Select * from KetQuaXetNghiem where ID = " + id;
+                    DbDataReader reader1 = cmd.ExecuteReader();
+                    reader1.Read();
+                    // ghi vào các cell trong bảng
+                    oTable.Cell(2, 1).Range.Text = "Urea";
+                    oTable.Cell(2, 2).Range.Text = reader1[reader1.GetOrdinal("Urea")].ToString();
+                    oTable.Cell(3, 1).Range.Text = "Creatinine";
+                    oTable.Cell(3, 2).Range.Text = reader1[reader1.GetOrdinal("Creatinine")].ToString();
+                    oTable.Cell(4, 1).Range.Text = "CPK";
+                    oTable.Cell(4, 2).Range.Text = reader1[reader1.GetOrdinal("CPK")].ToString();
+                    oTable.Cell(5, 1).Range.Text = "Calcium";
+                    oTable.Cell(5, 2).Range.Text = reader1[reader1.GetOrdinal("Calcium")].ToString();
+                    oTable.Cell(6, 1).Range.Text = "Phosphorous";
+                    oTable.Cell(6, 2).Range.Text = reader1[reader1.GetOrdinal("Phosphorous")].ToString();
+                    oTable.Cell(7, 1).Range.Text = "Amylase";
+                    oTable.Cell(7, 2).Range.Text = reader1[reader1.GetOrdinal("Amylase")].ToString();
+                    oTable.Cell(8, 1).Range.Text = "Lipase";
+                    oTable.Cell(8, 2).Range.Text = reader1[reader1.GetOrdinal("Lipase")].ToString();
+                    oTable.Cell(9, 1).Range.Text = "Bilirubin Toàn Phần";
+                    oTable.Cell(9, 2).Range.Text = reader1[reader1.GetOrdinal("BilirubinToanPhan")].ToString();
+                    oTable.Cell(10, 1).Range.Text = "AST";
+                    oTable.Cell(10, 2).Range.Text = reader1[reader1.GetOrdinal("AST")].ToString();
+                    oTable.Cell(11, 1).Range.Text = "ALT";
+                    oTable.Cell(11, 2).Range.Text = reader1[reader1.GetOrdinal("ALT")].ToString();
+                    oTable.Cell(12, 1).Range.Text = "AlkalinePhosphatase";
+                    oTable.Cell(12, 2).Range.Text = reader1[reader1.GetOrdinal("AlkalinePhosphatase")].ToString();
+                    oTable.Cell(13, 1).Range.Text = "Glucose";
+                    oTable.Cell(13, 2).Range.Text = reader1[reader1.GetOrdinal("Glucose")].ToString();
+
+                    oWord.Selection.Move(Word.WdUnits.wdLine, 200); // dịch con trỏ đến cuối văn bản (bỏ qua table)
+                    oWord.Selection.Font.Italic = 1;
+                    oWord.Selection.Paragraphs.LeftIndent = 250; // dịch lề bên trái của đoạn văn này
+                    oWord.Selection.Paragraphs.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                    oWord.Selection.ParagraphFormat.SpaceAfter = 8;
+                    oWord.Selection.TypeText("Hà Nội, ngày " + ngay_co_kq.Day.ToString() + ", tháng " + ngay_co_kq.Month.ToString() + ", năm " + ngay_co_kq.Year.ToString() + ".\r\nChữ ký bác sĩ\r\n");
+                    oWord.Visible = true;
+                }
+                else
+                {
+                    Console.WriteLine("Khong tim thay");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
